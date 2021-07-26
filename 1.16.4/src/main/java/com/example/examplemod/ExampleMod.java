406772673a9799e85f3ea44ac6d69778d82429f2
hohserg <hohserg1@yandex.ru>
2021-07-26 17:20:23 +0300

    1.16 done

--- 1.16.4/src/main/java/com/example/examplemod/ExampleMod.java
+++ 1.16.4/src/main/java/com/example/examplemod/ExampleMod.java
@@ -1,7 +1,13 @@
 package com.example.examplemod;
 
+import com.example.examplemod.network.ExamplePacket1;
+import net.java.games.input.Component;
+import net.java.games.input.Keyboard;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.settings.KeyBinding;
+import net.minecraftforge.client.event.InputEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -18,67 +24,20 @@ import org.apache.logging.log4j.Logger;
 
 import java.util.stream.Collectors;
 
-// The value here should match an entry in the META-INF/mods.toml file
 @Mod("examplemod")
-public class ExampleMod
-{
-    // Directly reference a log4j logger.
-    private static final Logger LOGGER = LogManager.getLogger();
+public class ExampleMod {
 
     public ExampleMod() {
-        // Register the setup method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
-        // Register the enqueueIMC method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::enqueueIMC);
-        // Register the processIMC method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::processIMC);
-        // Register the doClientStuff method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
-
-        // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
     }
 
-    private void setup(final FMLCommonSetupEvent event)
-    {
-        // some preinit code
-        LOGGER.info("HELLO FROM PREINIT");
-        LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
-    }
-
-    private void doClientStuff(final FMLClientSetupEvent event) {
-        // do something that can only be done on the client
-        LOGGER.info("Got game settings {}", event.getMinecraftSupplier().get().options);
-    }
-
-    private void enqueueIMC(final InterModEnqueueEvent event)
-    {
-        // some example code to dispatch IMC to another mod
-        InterModComms.sendTo("examplemod", "helloworld", () -> { LOGGER.info("Hello world from the MDK"); return "Hello world";});
-    }
-
-    private void processIMC(final InterModProcessEvent event)
-    {
-        // some example code to receive and process InterModComms from other mods
-        LOGGER.info("Got IMC {}", event.getIMCStream().
-                map(m->m.getMessageSupplier().get()).
-                collect(Collectors.toList()));
-    }
-    // You can use SubscribeEvent and let the Event Bus discover methods to call
     @SubscribeEvent
-    public void onServerStarting(FMLServerStartingEvent event) {
-        // do something when the server starts
-        LOGGER.info("HELLO from server starting");
-    }
-
-    // You can use EventBusSubscriber to automatically subscribe events on the contained class (this is subscribing to the MOD
-    // Event bus for receiving Registry Events)
-    @Mod.EventBusSubscriber(bus=Mod.EventBusSubscriber.Bus.MOD)
-    public static class RegistryEvents {
-        @SubscribeEvent
-        public static void onBlocksRegistry(final RegistryEvent.Register<Block> blockRegistryEvent) {
-            // register a new block here
-            LOGGER.info("HELLO from Register Block");
-        }
+    public void onKeyPressed(InputEvent.KeyInputEvent event) {
+        System.out.println("onKeyPressed");
+        for (KeyBinding key : Minecraft.getInstance().options.keyHotbarSlots)
+            if (key.isDown()) {
+                new ExamplePacket1(key.getKey().getValue()).sendToServer();
+                return;
+            }
     }
 }
---
+++ 1.16.4/src/main/java/com/example/examplemod/network/ExamplePacket1.java
@@ -0,0 +1,21 @@
+package com.example.examplemod.network;
+
+import hohserg.elegant.networking.api.ClientToServerPacket;
+import hohserg.elegant.networking.api.ElegantPacket;
+import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraftforge.fml.DistExecutor;
+
+@ElegantPacket
+public class ExamplePacket1 implements ClientToServerPacket {
+    final int keyCode;
+
+    public ExamplePacket1(int keyCode) {
+        this.keyCode = keyCode;
+    }
+
+    @Override
+    public void onReceive(ServerPlayerEntity player) {
+        System.out.println("ExamplePacket1#onReceive keyCode: " + keyCode);
+        new ExamplePacket2(player.level.random.nextInt(10)).sendToPlayer(player);
+    }
+}
